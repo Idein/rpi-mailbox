41dc3be3be41b0bb60490595f6ed749cc0466abe
eldesh <takayuki@idein.jp>
2018-12-20 01:53:31 +0900

    add: high level property apis
    Add`info` example which shows some simple properties.

---
+++ examples/info.rs
@@ -0,0 +1,32 @@
+extern crate chrono;
+extern crate nix;
+extern crate rpi_mailbox;
+
+use chrono::prelude::*;
+use rpi_mailbox::*;
+
+fn main() {
+    let mb = Mailbox::new("/dev/vcio").expect("mailbox");
+
+    let rev = firmware_revision(&mb).expect("firmware_revision");
+    let date = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(rev as i64, 0), Utc);
+    println!("Firmware revision: {}", date.format("%b %e %Y %T"));
+
+    let model = get_board_model(&mb).expect("board_model");
+    println!("Board model: 0x{:08x}", model);
+
+    let rev = get_board_revision(&mb).expect("board_revision");
+    println!("Board revision: 0x{:08x}", rev);
+
+    let mac = get_board_mac_address(&mb).expect("board_mac_address");
+    println!("Board MAC address: {:012x}", mac);
+
+    let serial = get_board_serial(&mb).expect("board_serial");
+    println!("Board serial: 0x{:x}", serial);
+
+    let (base, size) = get_arm_memory(&mb).expect("arm_memory");
+    println!("ARM memory: 0x{:08x} bytes at 0x{:08x}", size, base);
+
+    let (base, size) = get_vc_memory(&mb).expect("arm_memory");
+    println!("VC memory:  0x{:08x} bytes at 0x{:08x}", size, base);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,123 @@ pub mod mailbox;
 pub mod message;
 pub mod raspberrypi_firmware;
 
+use std::mem::size_of;
+
+use kernel::rpi_firmware_property;
 pub use mailbox::Mailbox;
+use raspberrypi_firmware::rpi_firmware_property_tag;
+
+pub fn firmware_revision(mb: &Mailbox) -> nix::Result<u32> {
+    use message::firmware_revision::*;
+    use rpi_firmware_property_tag::*;
+
+    let mut msg = Message { in_: In };
+    rpi_firmware_property(
+        mb,
+        RPI_FIRMWARE_GET_FIRMWARE_REVISION,
+        &mut msg as *mut Message as *mut u8,
+        size_of::<Message>(),
+        size_of::<Out>(),
+    );
+    unsafe { Ok(msg.out.firmware_revision) }
+}
+
+pub fn get_board_model(mb: &Mailbox) -> nix::Result<u32> {
+    use message::board_model::*;
+    use rpi_firmware_property_tag::*;
+
+    let mut msg = Message { in_: In };
+    rpi_firmware_property(
+        mb,
+        RPI_FIRMWARE_GET_BOARD_MODEL,
+        &mut msg as *mut Message as *mut u8,
+        size_of::<Message>(),
+        size_of::<Out>(),
+    );
+    unsafe { Ok(msg.out.board_model) }
+}
+
+pub fn get_board_revision(mb: &Mailbox) -> nix::Result<u32> {
+    use message::board_revision::*;
+    use rpi_firmware_property_tag::*;
+
+    let mut msg = Message { in_: In };
+    rpi_firmware_property(
+        mb,
+        RPI_FIRMWARE_GET_BOARD_REVISION,
+        &mut msg as *mut Message as *mut u8,
+        size_of::<Message>(),
+        size_of::<Out>(),
+    );
+    unsafe { Ok(msg.out.board_revision) }
+}
+
+pub fn get_board_mac_address(mb: &Mailbox) -> nix::Result<u64> {
+    use message::board_mac_address::*;
+    use rpi_firmware_property_tag::*;
+
+    let mut msg = Message { in_: In(0) };
+    rpi_firmware_property(
+        mb,
+        RPI_FIRMWARE_GET_BOARD_MAC_ADDRESS,
+        &mut msg as *mut Message as *mut u8,
+        size_of::<Message>(),
+        size_of::<Out>(),
+    );
+    unsafe {
+        Ok((msg.out.v0 as u64) << 8 * 5
+            | (msg.out.v1 as u64) << 8 * 4
+            | (msg.out.v2 as u64) << 8 * 3
+            | (msg.out.v3 as u64) << 8 * 2
+            | (msg.out.v4 as u64) << 8 * 1
+            | (msg.out.v5 as u64) << 8 * 0)
+    }
+}
+
+pub fn get_board_serial(mb: &Mailbox) -> nix::Result<u64> {
+    use message::board_serial::*;
+    use rpi_firmware_property_tag::*;
+
+    let mut msg = Message { in_: In };
+    rpi_firmware_property(
+        mb,
+        RPI_FIRMWARE_GET_BOARD_SERIAL,
+        &mut msg as *mut Message as *mut u8,
+        size_of::<Message>(),
+        size_of::<Out>(),
+    );
+    unsafe { Ok(msg.out.board_serial) }
+}
+
+pub fn get_arm_memory(mb: &Mailbox) -> nix::Result<(u32, u32)> {
+    use message::arm_memory::*;
+    use rpi_firmware_property_tag::*;
+
+    let mut msg = Message { in_: In };
+    rpi_firmware_property(
+        mb,
+        RPI_FIRMWARE_GET_ARM_MEMORY,
+        &mut msg as *mut Message as *mut u8,
+        size_of::<Message>(),
+        size_of::<Out>(),
+    );
+    unsafe { Ok((msg.out.base, msg.out.size)) }
+}
+
+pub fn get_vc_memory(mb: &Mailbox) -> nix::Result<(u32, u32)> {
+    use message::vc_memory::*;
+    use rpi_firmware_property_tag::*;
+
+    let mut msg = Message { in_: In };
+    rpi_firmware_property(
+        mb,
+        RPI_FIRMWARE_GET_VC_MEMORY,
+        &mut msg as *mut Message as *mut u8,
+        size_of::<Message>(),
+        size_of::<Out>(),
+    );
+    unsafe { Ok((msg.out.base, msg.out.size)) }
+}
 
 #[cfg(test)]
 mod tests {
