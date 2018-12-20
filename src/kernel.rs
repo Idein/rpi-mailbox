0676e4ed84dbdfa153d30e75b79fa8a4a93f99cd
eldesh <takayuki@idein.jp>
2018-12-20 13:52:28 +0900

    cleanup

--- src/kernel.rs
+++ src/kernel.rs
@@ -31,6 +31,7 @@ fn rpi_firmware_property_list(mb: &Mailbox, data: *mut u8, tag_size: usize) -> R
 
     let size: usize = size_of::<u32>() * 2 + tag_size + size_of::<u32>();
     debug!("{}:{}", size, tag_size);
+
     let mut buf: Vec<u32> = vec![0u32; size / 4];
     buf[0] = size as u32;
     buf[1] = RPI_FIRMWARE_STATUS_REQUEST as u32;
@@ -38,13 +39,12 @@ fn rpi_firmware_property_list(mb: &Mailbox, data: *mut u8, tag_size: usize) -> R
         ptr::copy(data, buf.as_mut_ptr().offset(2) as *mut u8, tag_size);
     }
     buf[size / 4 - 1] = RPI_FIRMWARE_PROPERTY_END as u32;
-    debug!("buf[{}]: ", buf.len());
-    buf.iter().for_each(|x| debug!("{},", x));
-    debug!("");
+
+    debug!("buf: {:?}", buf);
 
     let res = unsafe { ioctl::mailbox_property(mb.as_raw_fd(), buf.as_mut_ptr()) }?;
 
-    debug!("buf[1]: {}", buf[1]);
+    debug!("buf: {:?}", buf);
     if buf[1] != RPI_FIRMWARE_STATUS_SUCCESS as u32 {
         return Err(ErrorKind::RequestFailed(buf[1]).into());
     }
@@ -86,12 +86,13 @@ pub fn rpi_firmware_property(
 
     rpi_firmware_property_list(mb, data.as_mut_ptr() as *mut u8, data_size)?;
 
-    debug!("req_resp_size: {:x}", data[2]);
+    debug!("data[..] {} {} {} {}", data[0], data[1], data[2], data[3]);
 
     if (data[2] & (1u32 << 31)) == 0 {
         return Err(ErrorKind::ReqRespSizeBit(data[2]).into());
     }
-    data[2] &= !(1u32 << 31);
+
+    data[2] &= !(1u32 << 31); // clear flag
 
     debug!("req_resp_size: {:x},{:x}", data[2], req_resp_size);
     if data[2] != req_resp_size as u32 {
@@ -109,7 +110,7 @@ pub fn rpi_firmware_property(
 
     unsafe {
         ptr::copy(
-            (data.as_ptr() as *mut u8)
+            (data.as_ptr() as *const u8)
                 .offset(size_of::<rpi_firmware_property_tag_header>() as isize),
             tag_data,
             req_resp_size,
